109d1726669b685078e6ee08dabb5fa59d6c4bd9
Zano187 <liebenbergb573@gmail.com>
2025-07-11 17:53:58 +0200

    Added Message Entity, MessageType & MessageStatus Enums

---
+++ src/main/java/za/ac/cput/linkup/domain/message/MessageStatus.java
@@ -0,0 +1,14 @@
+package za.ac.cput.linkup.domain.message;
+
+/* MessageStatus.java
+MessageStatus enum
+Author: Brezano Liebenberg (230463886)
+Date:9 May 2025
+*/
+
+public enum MessageStatus {
+    SENT,
+    DELIVERED,
+    FAILED,
+    PENDING;
+}
---
+++ src/main/java/za/ac/cput/linkup/domain/message/MessageType.java
@@ -0,0 +1,16 @@
+package za.ac.cput.linkup.domain.message;
+
+/* MessageType.java
+MessageType enum
+Author: Brezano Liebenberg (230463886)
+Date:9 May 2025
+*/
+
+public enum MessageType {
+
+    TEXT,
+    IMAGE,
+    VIDEO,
+    GIF,
+    VOICENOTE;
+}
