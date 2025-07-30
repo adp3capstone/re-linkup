847733faac036103c19c8310404edd899918ad0c
Seja.Kgomokaboya <sejahope63@gmail.com>
2025-07-30 09:48:41 +0200

    UserRepository

--- src/main/java/za/ac/cput/linkup/repository/UserRepository.java
+++ src/main/java/za/ac/cput/linkup/repository/UserRepository.java
@@ -1,8 +1,24 @@
 package za.ac.cput.linkup.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.linkup.domain.User;
-import za.ac.cput.linkup.domain.message.Message;
+import za.ac.cput.linkup.domain.enums.Gender;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+Optional<User> findByUsername(String username);
+Optional<User> findByLastName(String lastName);
+Optional<User> findByAge(String age);
+
+ Optional<User> findByFirstName(String firstName);
+
+ Optional<User> findByGender(Gender gender);
+
+Optional<User> findByYearOfStudy(String yearOfStudy);
+//
 }
+////
